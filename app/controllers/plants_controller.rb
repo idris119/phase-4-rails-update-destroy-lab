c0335bec7cddae05343cc806e184c5f98fab76eb
Joseph Mburu <josephmacharia575@gmail.com>
2023-06-25 22:38:45 +0300

    COMPLETED

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -17,6 +17,29 @@ class PlantsController < ApplicationController
     plant = Plant.create(plant_params)
     render json: plant, status: :created
   end
+  #destroy /plants
+  def destroy
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.destroy
+      head :no_content
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+  #update plants 
+
+  def update
+    plant = Plant.find_by(id: params[:id])
+    if plant
+      plant.update(plant_params)
+      render json: plant
+    else
+      render json: { error: "Plant not found" }, status: :not_found
+    end
+  end
+
+
 
   private
 
